6c47e13e37ef6c5745ace6b7f6ebc14194eaf6fa
Lothar Tjipueja <lothart@deepcatch.com.na>
2022-09-24 13:18:24 +0200

    'font-end'

---
+++ app.js
@@ -0,0 +1,11 @@
+function search() {
+ document.querySelector('.search').style.display = 'block' 
+
+  if (block) {
+    document.querySelector('.search').style.display = 'none' 
+  }
+
+  console.log(showing)
+}
+
+//setTimeout(() => https.remove(), 3000);
